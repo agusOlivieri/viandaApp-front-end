c6d1a6d9a3ddcc83264a4f8240cda0d538a8df84
agus <agustinolivieri30@gmail.com>
2025-01-06 15:52:37 -0300

    HomeView

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <RouterView />
+  <div class="container h-screen flex flex-col bg-sky-200">
+    <RouterView />
+  </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <script setup>
-
+import PedirViandaBtn from '@/components/PedirViandaBtn.vue';
 </script>
 
 <template>
-  <h1 class="text-3xl">Hello from Vue!</h1>
+  <section class="flex justify-center items-center w-full py-4">
+    <h1 class="text-3xl font-bold">¡Ordene su comida!</h1>
+  </section>
+  <section class="flex flex-grow justify-center items-center">
+    <PedirViandaBtn />
+  </section>
 </template>
