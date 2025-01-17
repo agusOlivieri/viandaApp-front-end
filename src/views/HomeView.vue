200485de854b69f53c974a3d7dd69d2d3522b5f7
agus <agustinolivieri30@gmail.com>
2025-01-16 21:13:39 -0300

    just so i dont lose the streak

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import PedirViandaBtn from '@/components/PedirViandaBtn.vue';
 
 <template>
   <section id="home" class="flex flex-col h-screen w-full py-4">
-    <h1 class="text-3xl font-bold text-center mt-4">¡Ordene su comida!</h1>
+    <h1 class="text-3xl font-bold text-center mt-4">¡Ordene su vianda!</h1>
     <div class="flex-grow flex justify-center items-center">
       <PedirViandaBtn />
     </div>
