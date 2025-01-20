096fb8dcae4071abc18b9452b1c10d68058d0a7d
agus <agustinolivieri30@gmail.com>
2025-01-20 11:08:50 -0300

    protected routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import NotFoundView from '@/views/NotFoundView.vue'
 import OrderView from '@/views/OrderView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import LoginView from '@/views/LoginView.vue'
+import { jwtDecode } from 'jwt-decode'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,7 @@ const router = createRouter({
       path: '/ordenar',
       name: 'ordenar',
       component: OrderView,
+      meta: { requiresAuth: true, roles: ["CLIENTE"] },
     },
     {
       path: '/:catchAll(.*)',
@@ -35,6 +37,28 @@ const router = createRouter({
     },
     
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("access_token");
+
+  if (to.meta.requiresAuth) {
+    if (!token) {
+      return next("/login");
+    }
+
+    const decodedToken = jwtDecode(token);
+    const userRole = decodedToken.role;
+    console.log("rol del token: ", userRole)
+    console.log("rol meta: ", to.meta.roles[0])
+
+    if (to.meta.roles && to.meta.roles[0] !== userRole) {
+      alert("No tienes permisos para acceder a esta página");
+      return next("/");
+    }
+  }
+
+  next();
 })
 
 export default router
