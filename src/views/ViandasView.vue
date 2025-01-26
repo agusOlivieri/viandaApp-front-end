e018496db041bd4f7746d5a7444c950969612290
agus <agustinolivieri30@gmail.com>
2025-01-26 20:36:40 -0300

    took out 'id' column in viandas view

--- src/views/ViandasView.vue
+++ src/views/ViandasView.vue
@@ -6,7 +6,7 @@ import ViandaAppTable from '@/components/ViandaAppTable.vue';
 <template>
     <section id="viandas">
         <div class="w-full flex justify-center">
-            <ViandaAppTable :columns="['id', 'nombre', 'descripcion', 'precio']" :endpoint="'http://localhost:8080/api/viandas'" :queryParams="{}" />
+            <ViandaAppTable :columns="['nombre', 'descripcion', 'precio']" :endpoint="'http://localhost:8080/api/viandas'" :queryParams="{}" />
         </div>
     </section>
 </template>
