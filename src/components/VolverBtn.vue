5b45ea31c19549e7d048f84947dc02a0c81bd35d
agus <agustinolivieri30@gmail.com>
2025-01-28 12:59:01 -0300

    Volver button

---
+++ src/components/VolverBtn.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const volver = () => {
+    router.back();
+}
+
+</script>
+
+<template>
+    <div>
+        <button @click="volver" class="bg-gray-500 text-white font-bold py-2 px-4 rounded-lg shadow hover:bg-gray600 transition">
+            Volver
+        </button>
+    </div>
+</template>
