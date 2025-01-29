83ebe78a655e1dca474addfd20e33f329352544d
agus <agustinolivieri30@gmail.com>
2025-01-29 20:07:54 -0300

    added VolverBtn to order view

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import ViandasListing from '@/components/ViandasListing.vue'
+import VolverBtn from '@/components/VolverBtn.vue';
 </script>
 
 <template>
     <ViandasListing />
+    <VolverBtn />
 </template>
