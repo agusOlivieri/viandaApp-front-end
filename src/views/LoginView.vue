c92f416990d7a65111ff5e6d963f2b16a1a1336c
agus <agustinolivieri30@gmail.com>
2025-01-31 18:40:19 -0300

    changed background in login and register views

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -37,7 +37,7 @@ const login = async () => {
 </script>
 
 <template>
-    <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div class="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div class="max-w-md w-full space-y-8 bg-white p-8 rounded-lg shadow-lg">
         <h2 class="text-center text-3xl font-extrabold text-gray-900">Iniciar Sesión</h2>
         <form @submit.prevent="login" class="mt-8 space-y-6">
