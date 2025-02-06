269a0200ee9fadfd8c3fd3327640135954536119
agus <agustinolivieri30@gmail.com>
2025-02-06 13:10:43 -0300

    EditBtn component

---
+++ src/components/EditBtn.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { DefineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+    id: {
+        type: Number,
+        required: true
+    }
+});
+
+const emit = defineEmits(["edit"]);
+
+const handleEdit = () => {
+    emit("edit", props.id);
+};
+
+</script>
+
+<template>
+    <button @click="handleEdit" class="bg-yellow-400 text-white px-3 py-1 rounded hover:bg-yellow-500">
+        Editar
+    </button>
+</template>
