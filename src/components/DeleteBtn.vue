fa70d4b988efcc27f5c3438193d4b31dfe70f51c
agus <agustinolivieri30@gmail.com>
2025-02-07 14:32:35 -0300

    DeleteBtn component

---
+++ src/components/DeleteBtn.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineEmits } from 'vue';
+
+const props = defineProps({
+    id: {
+        type: Number,
+        required: true
+    }
+});
+
+const emit = defineEmits(["delete"]);
+
+const handleDelete = () => {
+    if (confirm("¿Seguro quiere eliminar esta vianda?")) {
+        emit("delete", props.id)
+    }
+}
+</script>
+
+<template>
+    <button @click="handleDelete" class="bg-red-400 px-3 py-1 rounded hover:bg-red-500">
+        Eliminar
+    </button>
+</template>
