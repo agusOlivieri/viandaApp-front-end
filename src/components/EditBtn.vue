ba036bfade13a221ade6e9f7ac4b66f1b79fa1ce
agus <agustinolivieri30@gmail.com>
2025-02-07 14:43:56 -0300

    changed text color to edit btn

--- src/components/EditBtn.vue
+++ src/components/EditBtn.vue
@@ -17,7 +17,7 @@ const handleEdit = () => {
 </script>
 
 <template>
-    <button @click="handleEdit" class="bg-yellow-400 text-white px-3 py-1 rounded hover:bg-yellow-500">
+    <button @click="handleEdit" class="bg-yellow-400 px-3 py-1 rounded hover:bg-yellow-500">
         Editar
     </button>
 </template>
