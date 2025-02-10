0e800c3ff026695108959c4bac3de3b4ef7bfb88
agus <agustinolivieri30@gmail.com>
2025-02-10 15:48:42 -0300

    pulir detalles

--- src/views/PedidosView.vue
+++ src/views/PedidosView.vue
@@ -1,12 +1,17 @@
 <script setup>
 import ViandaAppTable from '@/components/ViandaAppTable.vue';
 import VolverBtn from '@/components/VolverBtn.vue';
+import { useUsuarioStore } from "@/stores/usuario";
+
+const usuarioStore = useUsuarioStore();
+
+const distribuidora = usuarioStore.getDistribuidora();
 </script>
 
 <template>
     <section id="viandas">
         <div class="w-full ">
-            <ViandaAppTable :columns="['cliente', 'vianda', 'precio', 'fecha']" :endpoint="'http://localhost:8080/api/pedidos'" :queryParams="{}" :acciones="false"/>
+            <ViandaAppTable :columns="['cliente', 'vianda', 'precio', 'fecha']" :endpoint="`http://localhost:8080/api/pedidos/${distribuidora}`" :queryParams="{}" :acciones="false"/>
         </div>
         <div>
             <VolverBtn />
