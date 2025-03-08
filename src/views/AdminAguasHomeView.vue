ff96927d488ecf24eb75a7e2e02103bad1f196c6
agus <agustinolivieri30@gmail.com>
2025-03-08 11:39:06 -0300

    fixed style for admin aguas home view

--- src/views/AdminAguasHomeView.vue
+++ src/views/AdminAguasHomeView.vue
@@ -4,7 +4,7 @@ import HomeViewBtn from '@/components/HomeViewBtn.vue';
 
 <template>
   <section id="home" class="flex flex-col h-screen w-full py-4">
-    <h1 class="text-3xl font-bold text-center mt-4">Vea los pedidos</h1>
+    <!-- <h1 class="text-3xl font-bold text-center mt-4">Vea los pedidos</h1> -->
     <div class="flex-grow flex justify-center items-center flex-col gap-4">
       <HomeViewBtn link="/admin/distribuidora/pedidos" linkText="Ver Pedidos" />
     </div>
